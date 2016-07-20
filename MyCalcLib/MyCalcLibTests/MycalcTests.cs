2c04d26b0f643d7c75f6b37629461b3f9da76a65
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-20 15:49:51 +0300

    Try#1

---
+++ MyCalcLib/MyCalcLibTests/MycalcTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLibTests
+	{
+	[TestClass]
+	public class MycalcTests
+		{
+		[TestMethod]
+		public void Sum_10and20_30returned ( )
+			{
+			//arrange
+
+			int x = 10;
+			int y = 20;
+			int expected = 30;
+
+			// act
+			
+			MyCalcLib.MyCalc c = new MyCalcLib.MyCalc ( );
+			int actual = c.Sum ( x, y );
+
+			//assert
+			Assert.AreEqual ( expected, actual );
+			}
+		}
+	}
---
+++ MyCalcLib/MyCalcLibTests1/MyCalcTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalcLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalcLib.Tests
+	{
+	[TestClass ( )]
+	public class MyCalcTests
+		{
+		[TestMethod ( )]
+		public void SumTest ( )
+			{
+			Assert.Fail ( );
+			}
+		}
+	}
