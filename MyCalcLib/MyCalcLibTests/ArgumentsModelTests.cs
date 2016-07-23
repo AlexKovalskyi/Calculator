189a5c974c927833c96e35b0ea92399e84b420a6
TommyWhite <moskalets.artem@gmail.com>
2016-07-23 19:32:56 +0300

    All unit test from ArgumentsModelTests class were fixed.

--- MyCalcLib/MyCalcLibTests/ArgumentsModelTests.cs
+++ MyCalcLib/MyCalcLibTests/ArgumentsModelTests.cs
@@ -16,57 +16,55 @@ namespace CalcLibTests
 
             Assert.AreEqual(expectedName, actualName);
         }
+        
+        [TestMethod]
+        public void CheckArgAPropeprty()
+        {
+            var args = Activator.CreateInstance(typeof(Arguments), 33,1 );
 
-        ////TODO: 2 Create property 'A' in the class with arguments.
-        //[TestMethod]
-        //public void CheckArgAPropeprty()
-        //{
-        //    var args = Activator.CreateInstance(typeof(Arguments));
+            var exists = false;
+            foreach (var prop in args.GetType().GetProperties())
+            {
+                if (prop.Name == "A")
+                {
+                    if (prop.PropertyType != typeof(int))
+                    {
+                        throw new Exception("Property is not type of int.");
+                    }
+                    exists = true;
+                }
+            }
+            Assert.IsTrue(exists, "A property was not found.");
+        }
+        
+        [TestMethod]
+        public void CheckArgBPropeprty()
+        {
+            var args = Activator.CreateInstance(typeof(Arguments), 33, 1);
 
-        //    var exists = false;
-        //    foreach (var prop in args.GetType().GetProperties())
-        //    {
-        //        if (prop.Name == "A")
-        //        {
-        //            if (prop.PropertyType != typeof(int))
-        //            {
-        //                throw new Exception("Property is not type of int.");
-        //            }
-        //        }
-        //    }
-        //    Assert.IsTrue(exists, "A property was not found.");
-        //}
+            var exists = false;
+            foreach (var prop in args.GetType().GetProperties())
+            {
+                if (prop.Name == "B")
+                {
+                    if (prop.PropertyType != typeof(int))
+                    {
+                        throw new Exception("Property is not type of int.");
+                    }
+                    exists = true;
+                }
+            }
 
-        ////TODO: 3 Create property 'B' in the class with arguments.
-        //[TestMethod]
-        //public void CheckArgBPropeprty()
-        //{
-        //    var args = Activator.CreateInstance(typeof(Arguments));
+            Assert.IsTrue(exists, "B property was not found.");
+        }
 
-        //    var exists = false;
-        //    foreach (var prop in args.GetType().GetProperties())
-        //    {
-        //        if (prop.Name == "B")
-        //        {
-        //            if (prop.PropertyType != typeof(int))
-        //            {
-        //                throw new Exception("Property is not type of int.");
-        //            }
-        //        }
-        //    }
+        [TestMethod]
+        public void CheckConstructor()
+        {
+            var args = (Arguments)Activator.CreateInstance(typeof(Arguments), 33, 33);
 
-        //    Assert.IsTrue(exists, "B property was not found.");
-        //}
-
-        //TODO: 4.1 Uncomment this after above tests are passing.
-        //TODO: 4.2 Create constructor with passing arguments 'a' and 'b' and assing them to properties.
-        //[TestMethod]
-        //public void CheckConstructor()
-        //{
-        //    var args = Activator.CreateInstance(typeof(UndefinedClass), 33, 33);
-
-        //    Assert.AreEqual(args.A, 33);
-        //    Assert.AreEqual(args.B, 33);
-        //}
+            Assert.AreEqual(args.A, 33);
+            Assert.AreEqual(args.B, 33);
+        }
     }
 }
