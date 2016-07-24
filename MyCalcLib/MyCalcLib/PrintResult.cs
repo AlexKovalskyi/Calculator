fedf9c92232767904b04c739dc7e95e0a08b3020
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-24 19:16:23 +0300

    TRY #1

---
+++ MyCalcLib/MyCalcLib/PrintResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+	public class PrintResult
+	{
+		
+		public void Print(int firstNumb, char operation, int secondNumb, int result)
+		{
+			Console.WriteLine("{0}  {1} {2} = {3}", firstNumb, operation, secondNumb, result);
+		}
+	}
+}
