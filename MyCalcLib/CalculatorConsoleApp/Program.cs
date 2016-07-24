802455006dcced8e341c4226ea0fdd4d405697ce
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-24 16:13:45 +0300

    Add new operations

--- MyCalcLib/CalculatorConsoleApp/Program.cs
+++ MyCalcLib/CalculatorConsoleApp/Program.cs
@@ -18,18 +18,59 @@ namespace CalculatorConsoleApp
        
         static void RunCalculator()
         {
-			Console.WriteLine("Calculator with can only sum");
-			Console.WriteLine("Write your first number: ");
-			int firstNumber = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine("Write your second number: ");
-			int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-			Arguments arguments = new Arguments(firstNumber, secondNumber);
-			Calculator calc = new Calculator();
-			int answer = calc.Sum(arguments);
+			int answer;
 			
-			Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, answer);
-			Console.ReadLine();
+			Console.WriteLine("Best of the best Calculator with 5 operation");
+			while (true)
+				{
+				try
+					{
+
+
+					Console.WriteLine("Enter one of the operation : +, -, *, / ,%");
+					char operation = Convert.ToChar(Console.ReadLine());
+					Console.WriteLine("Write your first number: ");
+					int firstNumber = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Write your second number: ");
+					int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+					Arguments arguments = new Arguments(firstNumber, secondNumber);
+					Calculator calc = new Calculator();
+					if (operation == '+')
+						{
+						answer = calc.Sum(arguments);
+						
+						Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, answer);
+						}
+					else if (operation == '/')
+						{
+						answer = calc.Devide(arguments);
+						Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, answer);
+						}
+					else if (operation == '*')
+						{
+						answer = calc.Multiply(arguments);
+						Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, answer);
+						}
+					else if (operation == '-')
+						{
+						answer = calc.Substract(arguments);
+						Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, answer);
+						}
+					else if (operation == '%')
+						{
+						answer = calc.Modulo(arguments);
+						Console.WriteLine("{0} % {1} = {2}", firstNumber, secondNumber, answer);
+						}
+						
+					}
+				catch (Exception ex)
+					{
+					Console.WriteLine("Error: " + ex.Message);
+					}
+				
+				}
 			}
+		
 		}
 }
