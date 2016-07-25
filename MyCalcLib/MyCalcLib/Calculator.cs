b2bf812529b60aecf2bf3aa96bec4eee3031dcb2
Alexandr Kovalskiy <alex.kowalskiy@gmail.com>
2016-07-25 19:24:55 +0300

    Updated class OutputServics

--- MyCalcLib/MyCalcLib/Calculator.cs
+++ MyCalcLib/MyCalcLib/Calculator.cs
@@ -17,51 +17,50 @@ namespace CalcLib
 
         public void RunCalculatorLoop()
         {
-            OutputService printOperationResult = new OutputService();
+            OutputService outputService = new OutputService();
             Console.WriteLine("Best of the best Calculator with 5 operations");
             while (true)
             {
-                OperationDelegate operationDelegate; //TODO: Remove declaring closer to usage. 
-                Console.WriteLine("Enter one of the operation : +, -, *, /, %"); //TODO: Remove to OutputService
-                char operation = Convert.ToChar(Console.ReadLine());
-                Console.WriteLine("Write your first number: ");//TODO: Remove to OutputService
+				outputService.PrintAvailableOperations();
+				char operation = Convert.ToChar(Console.ReadLine());
+				outputService.RequestFirstNumber();
                 int firstNumber = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Write your second number: ");//TODO: Remove to OutputService
+				outputService.RequestSecondNumber();
                 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
                 Arguments arguments = new Arguments(firstNumber, secondNumber);
-                Calculator calc = new Calculator(); //TODO: Critical implementation issue. Think, why?
-                switch (operation)
+				OperationDelegate operationDelegate; //TODO: Remove declaring closer to usage.
+				switch (operation)
                 {
-                    case ('+'): //TODO: Remove '(' and ')' characters
-                        operationDelegate = calc.Sum;
+                    case '+':
+                        operationDelegate = Sum;
                         break;
 
-                    case ('/'):
-                        operationDelegate = calc.Devide;
+                    case '/':
+                        operationDelegate = Devide;
                         break;
 
-                    case ('*'):
-                        operationDelegate = calc.Multiply;
+                    case '*':
+                        operationDelegate = Multiply;
                         break;
 
-                    case ('-'):
-                        operationDelegate = calc.Substract;
+                    case '-':
+                        operationDelegate = Substract;
                         break;
 
-                    case ('%'):
-                        operationDelegate = calc.Modulo;
+                    case '%':
+                        operationDelegate = Modulo;
                         break;
 
                     default:
-                        Console.WriteLine("invalid operation"); //TODO: Rename to "Invalid operation."
+                        Console.WriteLine("Invalid operation");
                         throw new Exception();
                 }
 
                 if (operationDelegate != null)
                 {
                     int result = operationDelegate.Invoke(arguments);
-                    printOperationResult.Print(arguments.A, operation, arguments.B, result);
+                    outputService.Print(arguments.A, operation, arguments.B, result);
                 }
             }
         }
--- MyCalcLib/MyCalcLib/OutputService.cs
+++ MyCalcLib/MyCalcLib/OutputService.cs
@@ -9,5 +9,25 @@ namespace MyCalcLib
 		{
 			Console.WriteLine("{0} {1} {2} = {3}", firstNumb, operation, secondNumb, result);
 		}
+		
+		
+		public void PrintAvailableOperations () 
+		{
+			Console.WriteLine("Enter one of the operation : +, -, *, /, %"); 
+
+		}
+
+
+		public void RequestFirstNumber()
+		{
+			Console.WriteLine("Write your first number: ");
+		}
+
+		public void RequestSecondNumber()
+		{
+			Console.WriteLine("Write your second number: ");
+		}
+
+
 	}
 }
